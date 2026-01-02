affd5cc76884f7dc95d9c410c5d8cc325de37fe6
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-01-02 16:42:35 +0530

    add image

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBar } from "../../component/nav-bar/nav-bar";
+import { Footer } from "../../component/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBar],
+  imports: [NavBar, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
